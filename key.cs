dc5a6938eb2cd4704688b5d8623c08eb2714c010
Marijn Baarda <Marijn Baarda>
2018-06-12 14:46:11 +0200

    Item/key toegevoegd

---
+++ key.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ZuulCS { 
+
+    public class Key : Item
+    {
+        public Key()
+        {
+            System.Console.WriteLine("Key Constructor");
+        }
+
+        public override void use(Object o)
+        {
+            if (o.GetType() == typeof(Room))
+            {
+                Room r = (Room)o; // must cast
+                //r.unlock();
+            }
+            else
+            {
+                // Object o is not a Room
+                System.Console.WriteLine("Can't use a key on this Object");
+            }
+
+        }
+
+        public override void use()
+        {
+            System.Console.WriteLine("Key::use()");
+        }
+
+    }
+}
